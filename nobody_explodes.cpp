abebd4dd5d4050b9761a94af6050e6af04edf415
David Anderson <jonie4@gmail.com>
2016-03-20 02:54:28 -0700

    Started working on serial

--- nobody_explodes.cpp
+++ nobody_explodes.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include "SerialClass.h"
 #include <string>
 using namespace std;
+//using namespace System;
+//using namespace System::IO::Ports;
 
 int main(int argc, char* argv[]){
-	string line;
+	// Open the logfile and ensure that it is open before continuing
 	ifstream logFile ("log\\ktane.log");
 	if( logFile.good() ){
-		while( logFile.good() ){
-			getline (logFile, line);
-		}
-		cout << "done" << endl;
-		logFile.close();
 	}
 	else cout << "Unable to open file" << endl;
+	
+	// Set up variables for serial communication to Arduino
+	string(answer);
+	string(portName)="COM6";
+	//int baudRate=9600;
+	
 	
 	return 0;
 }
