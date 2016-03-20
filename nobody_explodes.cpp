be692e9eb4d49f25d361f45ccb1a1ba8d9d5a9ea
David Anderson <jonie4@gmail.com>
2016-03-20 01:48:19 -0700

    Input Arguments for main function
    Refer back to this if input parameters are needed for the file.

--- nobody_explodes.cpp
+++ nobody_explodes.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
+	cout << argc << endl;
+	if (argc == 1)
+		cout << "Real mode" << endl;
+	else if(static_cast<string>(argv[1]) == "test"){
+		cout << "Test Mode" << endl;
+	}
+	else{
+		cout << "Not a valid mode" << endl;
+		return 0;
+	}
+	//ifstream fin;
+	//fin.open("logfile");
 	return 0;
 }
